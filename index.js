4f740868de7906c14c01a6847d614c7060e913ff
Avishek Shrestha <avishek.shrestha16@gmail.com>
2023-09-03 20:28:50 +0545

    connected database using mongoose

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const indexRouter = require("./routes");
 
+mongoose.connect("mongodb://127.0.0.1:27017/todoapp").then(() => {
+    console.log("Database connected.");
+});
 
 app.use(express.json());
 
