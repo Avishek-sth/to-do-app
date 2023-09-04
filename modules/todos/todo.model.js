d835b802ac17bb2c8bf028defb96e53c78ff86b3
Avishek Shrestha <avishek.shrestha16@gmail.com>
2023-09-04 06:50:45 +0545

    model schema created

--- modules/todos/todo.model.js
+++ modules/todos/todo.model.js
@@ -1,7 +1,14 @@
-const { Schema, model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-
-const todoSchema = new Schema({});
-
+const todoSchema = new Schema({
+  title: {type: String, required: true},
+  status: {
+    type: String,
+    enum: ["pending", "completed"],
+    required: true,
+    default: "pending",
+  },
+  created_at: {type: Date, default: Date.now },
+});
 
 module.exports = mongoose.model("Todo", todoSchema);
