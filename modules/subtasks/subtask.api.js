bcb3a2db9459a928e64cafb50ec1ac67ed7d09df
Avishek Shrestha <avishek.shrestha16@gmail.com>
2023-09-04 15:40:39 +0545

    created subtask router and connected to controller

--- modules/subtasks/subtask.api.js
+++ modules/subtasks/subtask.api.js
@@ -1,6 +1,58 @@
 const router = require("express").Router();
+const SubtaskController = require("./subtask.controller");
 
+//create
+router.post("/", async (req, res, next) => {
+  try {
+    const result = await SubtaskController.create(req.body);
+    res.json({ data: result, msg: "success" });
+  } catch (e) {
+    next(e);
+  }
+});
+//list
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await SubtaskController.list();
+    res.json({ data: result, msg: "success" });
+  } catch (e) {
+    next(e);
+  }
+});
 
+//getById
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await SubtaskController.getById(id);
+    res.json({ data: result, msg: "success" });
+  } catch (e) {
+    next(e);
+  }
+});
 
+//updateById
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await SubtaskController.updateById(id, req.body);
+    res.json({ data: result, msg: "success" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+//updateStatus
+
+//removeById
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const result = await SubtaskController.removeById(id);
+        res.json({data: result, msg:"success"});
+    } catch (e) {
+        next (e);
+    }
+});
 
 module.exports = router;
