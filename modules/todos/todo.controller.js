867382062d2bb8470af31bee2cce68ac4799c8cc
Avishek Shrestha <avishek.shrestha16@gmail.com>
2023-09-05 17:50:35 +0545

    controller setup completed

--- modules/todos/todo.controller.js
+++ modules/todos/todo.controller.js
@@ -1,9 +1,30 @@
 const TodoModel = require("./todo.model");
 
 
-const create = (payload) => {
-    return TodoModel.create(payload);
+const create = async (payload) => {
+    return await TodoModel.create(payload);
 };
 
+const list = async () => {
+    return await Model.find();
+}
 
-module.exports = {create};
+const getById = async (id) => {
+    return await Model.findOne({_id: id});
+}
+
+const updateById = async (id, payload) => {
+    return await Model.findOneAndUpdate({_id: id}, payload, { new: true});
+};
+
+const updateStatus = async (id, payload) => {
+    const {status} = payload;
+    if (!status) throw new Error("Status is required.");
+    return await Model.findOneAndUpdate({_id: id}, {status}, {new: true});
+};
+
+const removeById = async (id) => {
+    return await Model.deleteOne({_id: id});
+};
+
+module.exports = {create, list, getById, updateById, updateStatus, removeById};
