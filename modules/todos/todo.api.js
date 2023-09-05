9256c4d7ec40dd27e84ff95c2a5279e9ccd8777e
Avishek Shrestha <avishek.shrestha16@gmail.com>
2023-09-05 17:49:32 +0545

    todo route completed

--- modules/todos/todo.api.js
+++ modules/todos/todo.api.js
@@ -1,6 +1,62 @@
 const router = require("express").Router();
+const TodoController = require("./todo.controller");
 
+router.post("/", async (req, res, next) => {
+  try {
+    const result = await TodoController.create(req.body);
+    res.json({ data: result, msg: "success" });
+  } catch (e) {
+    next(e);
+  }
+});
 
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await TodoController.list();
+    res.json({ data: result, msg: "success" });
+  } catch (e) {
+    next(e);
+  }
+});
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await TodoController.getById(id);
+    res.json({ data: result, msg: "success" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await TodoController.updateById(id, req.body);
+    res.json({ data: result, msg: "success" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.patch("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await TodoController.updateStatus(id, req.body);
+    res.json({ data: result, msg: "success" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await TodoController.removeById(id);
+    res.json({ data: result, msg: "success" });
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = router;
